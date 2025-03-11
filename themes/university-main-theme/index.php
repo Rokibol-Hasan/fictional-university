b2bb6b0e08472ded3a2f3473dbc3e0cf32a43a11
Rokibol-Hasan <rokibol.hasancse@gmail.com>
2025-03-11 11:59:57 +0600

    loop understanding

--- themes/university-main-theme/index.php
+++ themes/university-main-theme/index.php
@@ -1,10 +1,26 @@
 <?php
 
-function greet($name,$color){
-    echo "<p>Hello $name, your favorite color is $color</p>";
+// function greet($name,$color){
+//     echo "<p>Hello $name, your favorite color is $color</p>";
+// }
+
+// greet("John","blue");
+
+
+$names = array('John','Jane','Joe');
+
+
+// for($i = 0; $i < count($names); $i++){
+//     echo "<p>Hello $names[$i]</p>";
+// }
+
+foreach($names as $name){
+    echo "<p>Hello $name</p>";
 }
 
-greet("John","blue");
 
 
 ?>
+
+<h1><?php echo bloginfo('name'); ?></h1>
+<p><?php echo bloginfo('description'); ?></p>
