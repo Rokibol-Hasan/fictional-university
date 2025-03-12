d43601ee520d38489c69b670544104685ca9b255
Rokibol-Hasan <rokibol.hasancse@gmail.com>
2025-03-12 09:33:22 +0600

    template setup

--- themes/university-main-theme/functions.php
+++ themes/university-main-theme/functions.php
@@ -1,6 +1,7 @@
 <?php 
 function universityResourceFiles(){
-    wp_enqueue_style('university_main_style',get_stylesheet_uri(''));
+    wp_enqueue_style('university_main_style',get_theme_file_uri('./build/style-index.css'));
+    wp_enqueue_style('university_extra_style',get_theme_file_uri('./build/index.css'));
 }
 
 add_action('wp_enqueue_scripts','universityResourceFiles');
