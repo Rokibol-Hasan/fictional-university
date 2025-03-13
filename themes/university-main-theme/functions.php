233f423a93055fef463bfae5abb0dd53c44b80cf
Rokibol-Hasan <rokibol.hasancse@gmail.com>
2025-03-13 11:51:52 +0600

    template updated

--- themes/university-main-theme/functions.php
+++ themes/university-main-theme/functions.php
@@ -6,8 +6,13 @@ function universityResourceFiles(){
     wp_enqueue_style('university_main_style',get_theme_file_uri('./build/style-index.css'));
     wp_enqueue_style('university_extra_style',get_theme_file_uri('./build/index.css'));
 }
-
 add_action('wp_enqueue_scripts','universityResourceFiles');
+
+
+function university_features(){
+    add_theme_support('title-tag');
+}
+add_action( 'after_setup_theme', 'university_features' );
 
 
 ?>
